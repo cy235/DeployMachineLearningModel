b86d85e199fbd659ede9ce14de29f7a29576bf14
cy235 <44735485+cy235@users.noreply.github.com>
2020-01-21 11:02:37 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 import requests
 # Change the value of experience that you want to test
-url = 'http://128.235.13.32:8000/api'
+url = 'http://104.247.55.102:8000/api'
 feature = [[5.8, 4.0, 1.2, 0.2]]
 labels ={
   0: "setosa",
